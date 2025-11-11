76845a084dc2284df45a3686bba0895dac33e7ce
Ekre Ceannmor <126588178+eceannmor@users.noreply.github.com>
2025-11-11 18:16:39 +0100

    half of the importer

--- include/otbv.h
+++ include/otbv.h
@@ -8,4 +8,5 @@ void save(const std::string &filename, const std::vector<bool> &data,
           const std::tuple<size_t, size_t, size_t> &resolution);      
 void save(const std::string &filename,
           const std::vector<std::vector<std::vector<bool>>> &data);
+std::vector<std::vector<std::vector<bool>>> load(const std::string &filename);
 }
--- src/io.h
+++ src/io.h
@@ -13,12 +13,10 @@ void stream_data_as_file_bytes(
 
 void save(const std::string &filename, const std::vector<bool> &data,
           const std::tuple<size_t, size_t, size_t> &resolution);
-          
+
 void save(const std::string &filename,
           const std::vector<std::vector<std::vector<bool>>> &data);
 
-// void save(std::string filename, vector3<bool> data);
-
-// void load();
+std::vector<std::vector<std::vector<bool>>> load(const std::string &filename);
 
 } // namespace otbv
