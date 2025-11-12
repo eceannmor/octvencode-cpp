9b8c751af5ec48231f0ee66411ed4b3b924549b2
Ekre Ceannmor <126588178+eceannmor@users.noreply.github.com>
2025-11-12 01:02:02 +0100

    e2e test, pass

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -11,22 +11,17 @@ int main() {
       0, 0, 0, 1, 1, 0, 1, 1, 1, //
       1, 1, 1, 1, 1, 1, 1, 1, 0  //
   };
-  bool expected_loaded_data[3][3][2] = {
-      {{0, 0}, {0, 1}, {1, 0}},
-      {{1, 1}, {1, 1}, {1, 1}},
-      {{1, 1}, {1, 1}, {1, 0}}};
+  bool expected_loaded_data[3][3][2] = {{{0, 0}, {0, 1}, {1, 0}},
+                                        {{1, 1}, {1, 1}, {1, 1}},
+                                        {{1, 1}, {1, 1}, {1, 0}}};
   std::tuple<size_t, size_t, size_t> resolution = {3, 3, 2};
   otbv::save(filename, data, resolution);
   std::vector<std::vector<std::vector<bool>>> loaded_data =
       otbv::load(filename);
-  printf("%zu, %zu, %zu\n", loaded_data.size(), loaded_data[0].size(),
-         loaded_data[0][0].size());
-
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       for (int k = 0; k < 2; k++) {
-        // assert(expected_loaded_data[i][j][k] == loaded_data[i][j][k]);
-        printf("%d %d\n", expected_loaded_data[i][j][k], bool(loaded_data.at(i).at(j).at(k)));
+        assert(expected_loaded_data[i][j][k] == loaded_data[i][j][k]);
       }
     }
   }
